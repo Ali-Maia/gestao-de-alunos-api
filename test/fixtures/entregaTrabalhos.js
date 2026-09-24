b9b8b9a2d83e5bc1f8d2234a9a1f5b0d68b689ee
Ali-Maia <aliciaengcomp@gmail.com>
2026-09-24 11:16:04 -0300

    test: ajustar cenarios de entrega e fixtures

--- test/fixtures/entregaTrabalhos.js
+++ test/fixtures/entregaTrabalhos.js
@@ -5,7 +5,7 @@ export default[
       "nome": "João Lima",
       "email": "joao.lima8690@example.com",
       "matricula": "J123456",
-      "senha": "123456"
+      "senha": "1a2b3c"
     },
     "dadosDisciplina": {
       "nome": "Pensamento Computacional",
@@ -17,5 +17,48 @@ export default[
       "descricao": "Resolução dos exercícios 01 ao 10." 
     },
     "statusCodeEsperado": 201,
+    "mensagemErroEsperada": null,
+  },
+  {
+    "testTittle": "Validar que retorna erro 400 ao tentar realizar o cadastro de uma nova entrega de trabalho ao informar senha incorreta",
+    "dadosAluno": {
+      "nome": "Augusto Lima",
+      "email": "augusto.lima8690@example.com",
+      "matricula": "A123456",
+      "senha": "123456",
+      "senhaIncorreta": "senha_incorreta",
+    },
+    "dadosDisciplina": {
+      "nome": "Lógica e Algoritmo",
+      "codigo": "LA0102",
+      "cargaHoraria": 80 
+    },
+    "dadosTrabalho": {
+      "titulo": "Lista de Exercícios 02",
+      "descricao": "Resolução dos exercícios 01 ao 10." 
+    },
+    "statusCodeEsperado": 401,
+    "mensagemErroEsperada": "Token de autenticação inválido ou expirado.",
+  },
+  {
+  "testTittle": "Validar que retorna erro 403 ao tentar realizar o cadastro de uma nova entrega de trabalho ao informar senha incorreta",
+    "dadosAluno": {
+      "nome": "Raissa Monteiro",
+      "email": "raissa.monteiro8690@example.com",
+      "emailOutroAluno": "ana.souza@example.com",
+      "matricula": "RM123456",
+      "senha": "123456",
+    },
+    "dadosDisciplina": {
+      "nome": "Automação de Testes",
+      "codigo": "AT0102",
+      "cargaHoraria": 90 
+    },
+    "dadosTrabalho": {
+      "titulo": "Lista de Exercícios 03",
+      "descricao": "Resolução dos exercícios 01 ao 10." 
+    },
+    "statusCodeEsperado": 403,
+    "mensagemErroEsperada": "Você só pode acessar os seus próprios dados.",
   },
 ]
