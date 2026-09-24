828cfab997737df5b0ec8997f3704cb764ab2e8f
Ali-Maia <aliciaengcomp@gmail.com>
2026-09-23 22:36:35 -0300

    test(auth): adicionar helpers de login e dotenv para autenticação

--- test/external/auth.external.test.js
+++ test/external/auth.external.test.js
@@ -3,21 +3,25 @@ import { expect } from 'chai';
 import { api } from '../helpers/api.js';
 import { loginAdmin, loginUsuario } from '../helpers/auth.js';
 import 'dotenv/config';
+import login from "../fixtures/auth.js";
 
 describe('POST /api/auth/login', () => {
 
-  it('deve retornar 200 e um token quando o admin informar e-mail e senha corretos', async () => {
-    const respostaLogin = await loginAdmin();
+  for (const caso of login){
+    it(caso.testTitle, async () => {
+      const credenciaisUsuario = caso.dadosUsuario;
 
-    expect(respostaLogin.status).to.equal(200);
-    expect(respostaLogin.body).to.have.property('token');
-  });
+      const respostaLogin = await loginUsuario(credenciaisUsuario.emailUsuario, credenciaisUsuario.senhaUsuario);
 
-    it('deve retornar 401 quando a senha informada for inválida', async () => {
-    const respostaLogin = await loginUsuario(process.env.ADMIN_EMAIL, 'senha incorreta');
-    
-    expect(respostaLogin.status).to.equal(401);
-    expect(respostaLogin.body.error).to.equal('E-mail ou senha inválidos.');
-  });
+      expect(respostaLogin.status).to.equal(caso.statusCodeEsperado);
+
+      if(caso.statusCodeEsperado === 200){
+        expect(respostaLogin.body).to.have.property('token');
+      };
+      if (caso.statusCodeEsperado >= 400 && caso.statusCodeEsperado < 500) {
+        expect(respostaLogin.body.error).to.equal(caso.mensagemErroEsperada);
+      }
+    });
+  }
 
 });
